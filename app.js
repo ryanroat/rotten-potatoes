edde2575d0af464a38508f0a96030efb6e5a5aa7
Ryan Roat <ryanroat@gmail.com>
2020-07-28 16:39:31 -0400

    set index route as all reviews

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ const PORT_SERVER = 3030;
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// OUR MOCK ARRAY OF PROJECTS
+const reviews = [
+    { title: 'Great Review', movieTitle: 'Batman II' },
+    { title: 'Awesome Movie', movieTitle: 'Titanic' }
+];
+
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars working...' });
+    res.render('reviews-index', { reviews });
+});
+
+// route to /reviews eventually /INDEX
+
+app.get('/reviews', (req, res) => {
+    res.render('reviews-index', { reviews });
 });
 
 app.listen(PORT_SERVER, () => {
