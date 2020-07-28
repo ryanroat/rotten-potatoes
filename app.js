587baa3788825cd0f4a4450dc5d890e76e25fcab
Ryan Roat <ryanroat@gmail.com>
2020-07-28 15:58:32 -0400

    use handlebars as view engine

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 const PORT_SERVER = 3030;
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home', { msg: 'Handlebars working...' });
 });
 
 app.listen(PORT_SERVER, () => {
