b332314320bd15e4bf1022b607e5777a89bf1698
Ryan Roat <ryanroat@gmail.com>
2020-08-03 11:17:32 -0400

    cleanup commented code

--- app.js
+++ app.js
@@ -39,58 +39,6 @@ app.use(express.urlencoded({ extended: true }));
 // override with POST having ?_method=DELETE or ?_method=PUT
 app.use(methodOverride('_method'));
 
-// // render form to add new review
-// app.get('/reviews/new', (req, res) => {
-//     res.render('reviews-new', { title: `New Review` });
-// });
-
-// // route to individual review
-// app.get('/reviews/:id', (req, res) => {
-//     Review.findById(req.params.id)
-//         .then(review => {
-//             res.render('reviews-show', { review });
-//         })
-//         .catch(err => {
-//             console.log(err.message);
-//         });
-// });
-
-// // create new review
-// app.post('/reviews', (req, res) => {
-//     Review.create(req.body)
-//         .then(review => {
-//             // redirect to new review after post
-//             res.redirect(`/reviews/${review._id}`);
-//         })
-//         .catch(err => {
-//             console.log(err.message);
-//         });
-// });
-
-// // edit review
-// app.get('/reviews/:id/edit', (req, res) => {
-//     Review.findById(req.params.id, (err, review) => {
-//         res.render('reviews-edit', { review, title: `Edit Review` });
-//     });
-// });
-
-// // PUT edited review
-// app.put('/reviews/:id', (req, res) => {
-//     Review.findByIdAndUpdate(req.params.id, req.body).then(review => {
-//         res.redirect(`/reviews/${review._id}`);
-//     });
-// });
-
-// app.delete('/reviews/:id', (req, res) => {
-//     Review.findByIdAndRemove(req.params.id)
-//         .then(review => {
-//             res.redirect('/');
-//         })
-//         .catch(err => {
-//             console.log(err.message);
-//         });
-// });
-
 app.listen(PORT_SERVER, () => {
     console.log(`app server started on port ${PORT_SERVER}.`);
 });
